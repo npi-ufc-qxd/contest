1f0ad882f2870e762a6be5de38601f3a04c932a4
Matheus Souza <matheussouzadecarvalho@gmail.com>
2016-06-16 09:09:04 -0300

    Projeto pronto para rodar em tomcat externo

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/ContestApplication.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/ContestApplication.java
@@ -2,11 +2,19 @@ package ufc.quixada.npi.contest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContestApplication {
+public class ContestApplication extends SpringBootServletInitializer{
+	private static Class<ContestApplication> applicationClass = ContestApplication.class;
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContestApplication.class, args);
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
 	}
 }
