007fe97f5d5da5d8213295f619d65b0df2b33ac2
Matheus Souza <matheussouzadecarvalho@gmail.com>
2016-06-20 08:29:27 -0300

    LDAP quase configurado, faltando apenas o authentication provider

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/ContestApplication.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/ContestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContestApplication extends SpringBootServletInitializer{
+public class ContestApplication extends SpringBootServletInitializer {
 	private static Class<ContestApplication> applicationClass = ContestApplication.class;
 
 	public static void main(String[] args) {
---
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/util/Constants.java
@@ -0,0 +1,14 @@
+package ufc.quixada.npi.contest.util;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Constants {
+	// LDAP - atributos presentes no arquivo application.properties
+	public static final String LDAP_URL = "ldap.url";
+	public static final String LDAP_BASE = "ldap.base";
+	public static final String LDAP_USER = "ldap.user";
+	public static final String LDAP_PASSWORD = "ldap.password";
+	public static final String LDAP_OU = "ldap.ou";
+	public static final String BASE = LDAP_URL;
+}
