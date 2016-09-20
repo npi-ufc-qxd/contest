b2bf01b87813a1a0b036337ea6b5f9f4a04b3512
LucasSales <lucasalesvieira2@gmail.com>
2016-09-20 10:52:35 -0300

    removendo importes desnecessários

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/AutorController.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/AutorController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
