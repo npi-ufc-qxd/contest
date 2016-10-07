142531036e4b06bcab4ef44de8c70bf125061fa0
LucasSales <lucasalesvieira2@gmail.com>
2016-10-07 10:13:41 -0300

    -Mapado /error para as views de erros -Criação de novas views de erros

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/ErrorController.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/ErrorController.java
@@ -1,14 +1,45 @@
 package ufc.quixada.npi.contest.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorController {
-
-	@RequestMapping("/403")
-	public String acessoNegado(){
-		return "403";
+public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController {
+	
+	private final static String ERROR_PATH = "/error";
+	
+	@Override
+	public String getErrorPath() {
+		return ERROR_PATH;
 	}
 	
+	@RequestMapping(value = ERROR_PATH)
+    public String errorHtml(HttpServletRequest request) {
+		if(getStatus(request) == HttpStatus.NOT_FOUND){
+			return "/error/404";	
+		}
+		if(getStatus(request) == HttpStatus.FORBIDDEN){
+			return "/error/403";	
+		}
+		if(getStatus(request) == HttpStatus.INTERNAL_SERVER_ERROR){
+			return "/error/500";
+		}
+		return "/error/erro";
+    }
+	public HttpStatus getStatus(HttpServletRequest request) {
+		Integer statusCode = (Integer) request
+				.getAttribute("javax.servlet.error.status_code");
+		if (statusCode == null) {
+			return HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		try {
+			return HttpStatus.valueOf(statusCode);
+		}
+		catch (Exception ex) {
+			return HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+	}
 }
