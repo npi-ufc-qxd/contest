bf204b80de93acec7260011076b77f50e41aa7f0
LucasSales <lucasalesvieira2@gmail.com>
2016-10-20 16:59:23 -0300

    -remocao importe desnecessário

--- aplicacao/contest/src/test/java/ufc/quixada/npi/contest/OrganizadorVisualizaDetalhesEventoSteps.java
+++ aplicacao/contest/src/test/java/ufc/quixada/npi/contest/OrganizadorVisualizaDetalhesEventoSteps.java
@@ -21,7 +21,6 @@ import cucumber.api.java.pt.Quando;
 import ufc.quixada.npi.contest.controller.EventoControllerOrganizador;
 import ufc.quixada.npi.contest.model.EstadoEvento;
 import ufc.quixada.npi.contest.model.Evento;
-import ufc.quixada.npi.contest.model.Papel;
 import ufc.quixada.npi.contest.model.Pessoa;
 import ufc.quixada.npi.contest.service.EventoService;
 import ufc.quixada.npi.contest.service.MessageService;
