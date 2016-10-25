7a79c1b9a9cc389a55c5c7afbb4a1838fc82630f
AllefLobo <allef.lobo@gmail.com>
2016-10-25 19:12:49 -0300

    Remoção de import desnecessário.

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/model/Evento.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/model/Evento.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.print.attribute.standard.DateTimeAtCompleted;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
