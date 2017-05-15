9f78482942538b4cd90ba126774da07d95b0f01d
CarlosQuixada <carlosquixada95@gmail.com>
2017-05-15 08:41:51 -0300

    Corrigido erros de sintaxe

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/SecaoController.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/SecaoController.java
@@ -21,7 +21,7 @@ import ufc.quixada.npi.contest.service.TrabalhoService;
 import ufc.quixada.npi.contest.util.Constants;
 
 @Controller
-@RequestMapping(value="/secao")
+@RequestMapping(value = "/secao")
 public class SecaoController {
 	@Autowired
 	private SecaoService secaoService;
@@ -31,87 +31,85 @@ public class SecaoController {
 	private EventoService eventoService;
 	@Autowired
 	private PessoaService pessoaService;
-		
-	@RequestMapping(value="/paginaSecao")
-	public String indexSecao(){
+
+	@RequestMapping(value = "/paginaSecao")
+	public String indexSecao() {
 		return "secao/indexSecao";
 	}
-	
-	@RequestMapping(value="/cadastrarSecaoForm",method= RequestMethod.GET)
-	public String cadastrarSecaoForm(Model model){
+
+	@RequestMapping(value = "/cadastrarSecaoForm", method = RequestMethod.GET)
+	public String cadastrarSecaoForm(Model model) {
 		List<Pessoa> pessoas = pessoaService.getPossiveisOrganizadores();
 		List<Evento> eventos = eventoService.buscarEventos();
-		model.addAttribute("pessoas",pessoas);
-		model.addAttribute("eventos",eventos);
+		model.addAttribute("pessoas", pessoas);
+		model.addAttribute("eventos", eventos);
 		return "secao/cadastroSecao";
 	}
-	
-	@RequestMapping(value="/cadastrarSecao")
-	public String cadastrarSecao(@RequestParam String nome_secao,@RequestParam String descricao,@RequestParam Long id_responsavel,@RequestParam Long id_evento){
+
+	@RequestMapping(value = "/cadastrarSecao")
+	public String cadastrarSecao(@RequestParam String nomeSecao, @RequestParam String descricao,
+			@RequestParam Long idResponsavel, @RequestParam Long idEvento) {
 		Secao secao = new Secao();
-		Pessoa responsavel = pessoaService.get(id_responsavel);
-		Evento evento = eventoService.buscarEventoPorId(id_evento);
-		
-		System.out.println(nome_secao);
-		System.out.println(descricao);
-		System.out.println(responsavel.getNome());
-		System.out.println(evento.getNome());
-		
-		secao.setNome(nome_secao);
+		Pessoa responsavel = pessoaService.get(idResponsavel);
+		Evento evento = eventoService.buscarEventoPorId(idEvento);
+
+		secao.setNome(nomeSecao);
 		secao.setDescricao(descricao);
 		secao.setResponsavel(responsavel);
 		secao.setEvento(evento);
-		
+
 		secaoService.addOrUpdate(secao);
 		return Constants.TEMPLATE_MEUS_EVENTOS_ORG;
 	}
-	
-	@RequestMapping(value="/secaoTrabalhos", method=RequestMethod.GET)
-	public String secaoTrabalhos(){
+
+	@RequestMapping(value = "/secaoTrabalhos", method = RequestMethod.GET)
+	public String secaoTrabalhos() {
 		return "secao/secaoTrabalhos";
 	}
-	
-	//FALTA A PAGINA ALTERAR SECAO
-	@RequestMapping(value="/alterarSecao/{id}")
-	public String alterarSecaoForm(@PathVariable("id") Long idSecao,Model model){
+
+	// FALTA A PAGINA ALTERAR SECAO
+	@RequestMapping(value = "/alterarSecao/{id}")
+	public String alterarSecaoForm(@PathVariable("id") Long idSecao, Model model) {
 		Secao secao = secaoService.get(idSecao);
 		model.addAttribute("secao", secao);
-		return "";	
+		return "";
 	}
-	
-	@RequestMapping(value="/alterarSecao")
-	public String alterarSecao(Secao secao){
+
+	@RequestMapping(value = "/alterarSecao")
+	public String alterarSecao(Secao secao) {
 		secaoService.addOrUpdate(secao);
 		return Constants.TEMPLATE_MEUS_EVENTOS_ORG;
 	}
-	
-	//FALTA A PAGINA DE LISTAR TRABALHOS SEÇÃO
-	@RequestMapping(value="/secao/{id}")
-	public String listarTrabalhoSecao(@PathVariable("id") Long idSecao,Model model){
+
+	// FALTA A PAGINA DE LISTAR TRABALHOS SEÇÃO
+	@RequestMapping(value = "/secao/{id}")
+	public String listarTrabalhoSecao(@PathVariable("id") Long idSecao, Model model) {
 		Secao secao = secaoService.get(idSecao);
 		List<Trabalho> trabalhos = secao.getTrabalhos();
 		model.addAttribute("trabalhos", trabalhos);
-		return"";
+		return "";
 	}
-	
-	//FALTA A PAGINA DE LISTAR TRABALHOS SEÇÃO
-	@RequestMapping(value="/excluirSecao/{id}")
-	public String excluirSecao(@PathVariable("id") Long idSecao){
+
+	// FALTA A PAGINA DE LISTAR TRABALHOS SEÇÃO
+	@RequestMapping(value = "/excluirSecao/{id}")
+	public String excluirSecao(@PathVariable("id") Long idSecao) {
 		secaoService.delete(idSecao);
 		return "";
 	}
-	
+
 	@RequestMapping("/excluirTrabalho/{idSecao}/{idTrabalho}")
-	public String excluirTrabalhoSecao(@PathVariable("idSecao") Long idSecao,@PathVariable("idTrabalho") Long idTrabalho){
+	public String excluirTrabalhoSecao(@PathVariable("idSecao") Long idSecao,
+			@PathVariable("idTrabalho") Long idTrabalho) {
 		Trabalho trabalho = trabalhoService.getTrabalhoById(idTrabalho);
-		secaoService.removerTrablahoSecao(idSecao,trabalho);
-		return "redirect:/secao/"+idSecao;
+		secaoService.removerTrablahoSecao(idSecao, trabalho);
+		return "redirect:/secao/" + idSecao;
 	}
-	
+
 	@RequestMapping("/adicionarTrabalho/{idSecao}/{idTrabalho}")
-	public String adicionarTrabalhoSecao(@PathVariable("idSecao") Long idSecao,@PathVariable("idTrabalho") Long idTrabalho){
+	public String adicionarTrabalhoSecao(@PathVariable("idSecao") Long idSecao,
+			@PathVariable("idTrabalho") Long idTrabalho) {
 		Trabalho trabalho = trabalhoService.getTrabalhoById(idTrabalho);
 		secaoService.adicionarTrabalhoSecao(idSecao, trabalho);
-		return "redirect:/secao/"+idSecao;
+		return "redirect:/secao/" + idSecao;
 	}
 }
