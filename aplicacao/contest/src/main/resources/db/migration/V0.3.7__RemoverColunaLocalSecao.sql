d793ef553d8b8cb79fbf7aa27c6904aa0e27c3f7
CarlosQuixada <carlosquixada95@gmail.com>
2017-05-15 08:28:52 -0300

    Corrigido problema "Não Existe relação hibernate_sequence"

--- aplicacao/contest/src/main/resources/db/migration/V0.3.7__RemoverColunaLocalSecao.sql
+++ aplicacao/contest/src/main/resources/db/migration/V0.3.7__RemoverColunaLocalSecao.sql
@@ -1,7 +1,4 @@
 ALTER TABLE secao DROP COLUMN local;
---
--- Name: secao_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
 CREATE SEQUENCE secao_id_seq
     START WITH 1
@@ -9,7 +6,6 @@ CREATE SEQUENCE secao_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-
 
 --
 -- Name: secao_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
@@ -24,3 +20,13 @@ ALTER SEQUENCE secao_id_seq OWNED BY secao.id;
 
 
 ALTER TABLE ONLY secao ALTER COLUMN id SET DEFAULT nextval('secao_id_seq'::regclass);
+
+SELECT pg_catalog.setval('secao_id_seq', 1, false);
+
+CREATE SEQUENCE hibernate_sequence
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE hibernate_sequence OWNER TO postgres;
