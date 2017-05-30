294e2e628f54faa010873dd3652962574803621f
Zarathon Maia <zarathonmaia@gmail.com>
2017-05-30 10:15:38 -0300

    Update LoginController.java

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/LoginController.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/controller/LoginController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -67,7 +66,6 @@ public class LoginController {
 	@RequestMapping(value = "/dashboard")
 	public String dashboard(Model model){
 		List<Evento> eventos = eventoService.buscarEventosAtivosEPublicos();
-		System.out.println(eventos.size());
 		model.addAttribute("eventosParaParticipar",eventos);
 		return "dashboard";
 	}
