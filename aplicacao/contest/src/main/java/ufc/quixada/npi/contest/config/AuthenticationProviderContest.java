8911ab1999f444b649801807eff9511a34f99ee1
Zarathon Maia <zarathonmaia@gmail.com>
2017-10-11 08:53:38 -0300

    Update AuthenticationProviderContest.java

--- aplicacao/contest/src/main/java/ufc/quixada/npi/contest/config/AuthenticationProviderContest.java
+++ aplicacao/contest/src/main/java/ufc/quixada/npi/contest/config/AuthenticationProviderContest.java
@@ -40,7 +40,7 @@ public class AuthenticationProviderContest implements AuthenticationProvider {
 		String cpf = authentication.getName();
 		String password = authentication.getCredentials().toString();
 
-	
+		final Usuario usuario = usuarioService.getByCpf(cpf);
 		pessoa = pessoaService.getByCpf(cpf);
 
 		if (pessoa != null) { // Pessoa existe na Base Local entra no trecho abaixo
@@ -49,10 +49,7 @@ public class AuthenticationProviderContest implements AuthenticationProvider {
 						pessoa.getAuthorities());
 			}
 				
-		} else {
-			final Usuario usuario = usuarioService.getByCpf(cpf);			
-			if (usuario != null && usuarioService.autentica(cpf, password)) { 
-		
+		} else if (usuario != null && usuarioService.autentica(cpf, password)) { 
 			
 			String encondedPassword = pessoaService.encodePassword(password);
 			pessoa = ContestUtil.convertUsuarioToPessoa(encondedPassword, usuario);
@@ -66,7 +63,6 @@ public class AuthenticationProviderContest implements AuthenticationProvider {
 
 			return new UsernamePasswordAuthenticationToken(pessoa, pessoaService.encodePassword(password),
 					pessoa.getAuthorities());
-			}
 		}
 
 		throw new BadCredentialsException(messageService.getMessage("LOGIN_INVALIDO"));
